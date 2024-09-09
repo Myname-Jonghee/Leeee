72d84239514ba46cc0835aeb0068c42addbd2cd0
Myname-Jonghee <jonghee4911@naver.com>
2024-09-09 16:06:42 +0900

    Create Login.cs

---
+++ Project2/Models/Login.cs
@@ -0,0 +1,10 @@
+namespace Project_Battery.Models
+{
+    public class Login
+    {
+       
+        public string UserName { get; set; }
+        
+        public string Password { get; set; }
+    }
+}
